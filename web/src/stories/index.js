cd7ef43cd843c359f4ca23cd9fbcca83543d75c8
Julian Lawrence <julianlawrence@gmail.com>
2018-11-17 13:27:45 -0800

    removed need for yarn

--- web/src/stories/index.js
+++ web/src/stories/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
 
-import { storiesOf } from '@storybook/react';
+import {storiesOf} from '@storybook/react';
 
 import App from '../App.js';
-import Example from '../components/Button.js';
 
-storiesOf('Major', module)
-  .add('app', () => <App />)
-;
-
-storiesOf('Button', module)
-  .add('button', () => <Example/>)
-;
+storiesOf('Major', module).add('App', () => <App />);
