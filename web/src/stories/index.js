f3b490c8556bbd8afe827a4ee3ac5d0a9231999b
Julian Lawrence <julianlawrence@gmail.com>
2018-11-17 12:42:07 -0800

    wip

--- web/src/stories/index.js
+++ web/src/stories/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import { storiesOf } from '@storybook/react';
 
 import App from '../App.js';
-
+import Example from '../components/Button.js';
 
 storiesOf('Major', module)
   .add('app', () => <App />)
 ;
 
 storiesOf('Button', module)
+  .add('button', () => <Example/>)
 ;
