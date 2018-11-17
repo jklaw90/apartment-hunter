81b1d53649aa6ab01bdab9f2971eee195cf68751
Julian Lawrence <julianlawrence@gmail.com>
2018-11-16 23:13:15 -0800

    basic react kit and storybook

---
+++ web/src/stories/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+
+import App from '../App.js';
+
+
+storiesOf('Major', module)
+  .add('app', () => <App />)
+;
+
+storiesOf('Button', module)
+;
