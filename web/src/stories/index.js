d1d63c13863debb147a0895bbf2fc612f6642bc1
Julian Lawrence <julianlawrence@gmail.com>
2018-11-17 15:05:40 -0800

    welll

--- web/src/stories/index.js
+++ web/src/stories/index.js
@@ -3,5 +3,8 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 
 import App from '../App.js';
+import ListingRow from '../components/ListingRow.js';
 
 storiesOf('Major', module).add('App', () => <App />);
+
+storiesOf('Listings', module).add('ListingRow', () => <ListingRow />);
